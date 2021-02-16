02a7b51c7428d733d635aad9983beec91bd16eae
Ramakrishnan83 <rama.r.krishnan@gmail.com>
2021-02-15 22:24:59 -0500

    intial commit

---
+++ src/main/java/com/ramdemo/customer/CustomerApplication.java
@@ -0,0 +1,20 @@
+package com.ramdemo.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+}
